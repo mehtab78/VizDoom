77a38fd3d314a43be175dd99e37f607e508bfbd8
mehtab78 <mehtab6722@gmail.com>
2025-04-15 15:51:30 +0500

    updated the setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 echo "Installing ViZDoom DRL dependencies..."
-pip install --upgrade pip
-pip install vizdoom gym
-pip install stable-baselines3[extra]
-pip install opencv-python matplotlib
+# Install game environment dependencies
+apt-get update
+apt-get install -y cmake libboost-all-dev libopencv-dev zlib1g-dev libsdl2-dev libglu1-mesa-dev libpng-dev
+
+# Clone and build ViZDoom manually
+git clone https://github.com/mwydmuch/ViZDoom.git
+cd ViZDoom
+mkdir build && cd build
+cmake ..
+make -j4
+cd ..
+pip install .
+
+# Now install rest
+pip install stable-baselines3[extra] opencv-python matplotlib
 pip install git+https://github.com/shakenes/vizdoomgym.git
 echo "✅ Setup complete!"
